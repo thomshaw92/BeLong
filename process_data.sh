e1f97119a00fdd8a13e738f2d741230131cd6c83
thomshaw92 <thomasbasilshaw@gmail.com>
2022-09-23 12:56:40 +1000

    updates to include QSM in proc

--- process_data.sh
+++ process_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#
+#THIS SCRIPT HAS TO BE RUN FROM THE SAME PLACE AS INPUT
 
 # Process data. This script is designed to be run in the folder for a single subject, however 'sct_run_batch' can be
 # used to run this script multiple times in parallel across a multi-subject BIDS dataset.
@@ -16,10 +16,10 @@
 
 # The following global variables are retrieved from the caller sct_run_batch
 # but could be overwritten by uncommenting the lines below:
-PATH_DATA_PROCESSED="/90days/uqtshaw/BeLong/iBeLong_SCT_BIDS/derivatives/data_processed"
-PATH_RESULTS="/90days/uqtshaw/BeLong/iBeLong_SCT_BIDS/derivatives/results"
-PATH_LOG="/90days/uqtshaw/BeLong/iBeLong_SCT_BIDS/derivatives/log"
-PATH_QC="/90days/uqtshaw/BeLong/iBeLong_SCT_BIDS/derivatives/qc"
+PATH_DATA_PROCESSED="/90days/uqtshaw/BeLong/BeLong_SCT_BIDS/data_processed"
+PATH_RESULTS="/90days/uqtshaw/BeLong/BeLong_SCT_BIDS/results"
+PATH_LOG="/90days/uqtshaw/BeLong/BeLong_SCT_BIDS/log"
+PATH_QC="/90days/uqtshaw/BeLong/BeLong_SCT_BIDS/qc"
 
 # BASH SETTINGS
 
@@ -33,59 +33,60 @@ trap "echo Caught Keyboard Interrupt within script. Exiting now.; exit" INT
 # CONVENIENCE FUNCTIONS
 # ======================================================================================================================
 label_if_does_not_exist() {
-  ###
-  #  This function checks if a manual label file already exists, then:
-  #     - If it does, copy it locally.
-  #     - If it doesn't, perform automatic labeling.
-  #   This allows you to add manual labels on a subject-by-subject basis without disrupting the pipeline.
-  ###
-  local file="$1"
-  local file_seg="$2"
-  # Update global variable with segmentation file name
-  FILELABEL="${file}_labels"
-  FILELABELMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILELABEL}-manual.nii.gz"
-  echo "Looking for manual label: $FILELABELMANUAL"
-  if [[ -e $FILELABELMANUAL ]]; then
-    echo "Found! Using manual labels."
-    rsync -avzh "$FILELABELMANUAL" "${FILELABEL}".nii.gz
-  else
-    echo "Not found. Proceeding with automatic labeling."
-    # Generate labeled segmentation
-    sct_label_vertebrae -i "${file}".nii.gz -s "${file_seg}".nii.gz -c t2 -qc "${PATH_QC}" -qc-subject "${SUBJECT}"
-    # Create labels in the cord at C3 and C5 mid-vertebral levels
-    sct_label_utils -i "${file_seg}"_labeled.nii.gz -vert-body 3,5 -o "${FILELABEL}".nii.gz
-  fi
+    ###
+    #  This function checks if a manual label file already exists, then:
+    #     - If it does, copy it locally.
+    #     - If it doesn't, perform automatic labeling.
+    #   This allows you to add manual labels on a subject-by-subject basis without disrupting the pipeline.
+    ###
+    local file="$1"
+    local file_seg="$2"
+    # Update global variable with segmentation file name
+    FILELABEL="${file}_labels"
+    FILELABELMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILELABEL}-manual.nii.gz"
+    echo "Looking for manual label: $FILELABELMANUAL"
+    if [[ -e $FILELABELMANUAL ]]; then
+	echo "Found! Using manual labels."
+	rsync -avzh "$FILELABELMANUAL" "${FILELABEL}".nii.gz
+    else
+	echo "Not found. Proceeding with automatic labeling."
+	# Generate labeled segmentation
+	sct_label_vertebrae -i "${file}".nii.gz -s "${file_seg}".nii.gz -c t2 -qc "${PATH_QC}" -qc-subject "${SUBJECT}"
+	# Create labels in the cord at C3 and C5 mid-vertebral levels
+	sct_label_utils -i "${file_seg}"_labeled.nii.gz -vert-body 3,5 -o "${FILELABEL}".nii.gz
+    fi
 }
 
 segment_if_does_not_exist() {
-  ###
-  #  This function checks if a manual spinal cord segmentation file already exists, then:
-  #    - If it does, copy it locally.
-  #    - If it doesn't, perform automatic spinal cord segmentation.
-  #  This allows you to add manual segmentations on a subject-by-subject basis without disrupting the pipeline.
-  ###
-  local file="$1"
-  local contrast="$2"
-  # Update global variable with segmentation file name
-  FILESEG="${file}_seg"
-  FILESEGMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILESEG}-manual.nii.gz"
-  echo
-  echo "Looking for manual segmentation: $FILESEGMANUAL"
-  if [[ -e $FILESEGMANUAL ]]; then
-    echo "Found! Using manual segmentation."
-    rsync -avzh "$FILESEGMANUAL" "${FILESEG}".nii.gz
-    sct_qc -i "${file}".nii.gz -s "${FILESEG}".nii.gz -p sct_deepseg_sc -qc ${PATH_QC} -qc-subject "${SUBJECT}"
-  else
-    echo "Not found. Proceeding with automatic segmentation."
-    # Segment spinal cord
-    sct_deepseg_sc -i "${file}".nii.gz -c "$contrast" -qc ${PATH_QC} -qc-subject "${SUBJECT}"
-  fi
+    ###
+    #  This function checks if a manual spinal cord segmentation file already exists, then:
+    #    - If it does, copy it locally.
+    #    - If it doesn't, perform automatic spinal cord segmentation.
+    #  This allows you to add manual segmentations on a subject-by-subject basis without disrupting the pipeline.
+    ###
+    local file="$1"
+    local contrast="$2"
+    # Update global variable with segmentation file name
+    FILESEG="${file}_seg"
+    FILESEGMANUAL="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${FILESEG}-manual.nii.gz"
+    echo
+    echo "Looking for manual segmentation: $FILESEGMANUAL"
+    if [[ -e $FILESEGMANUAL ]]; then
+	echo "Found! Using manual segmentation."
+	rsync -avzh "$FILESEGMANUAL" "${FILESEG}".nii.gz
+	sct_qc -i "${file}".nii.gz -s "${FILESEG}".nii.gz -p sct_deepseg_sc -qc ${PATH_QC} -qc-subject "${SUBJECT}"
+    else
+	echo "Not found. Proceeding with automatic segmentation."
+	# Segment spinal cord
+	sct_deepseg_sc -i "${file}".nii.gz -c "$contrast" -qc ${PATH_QC} -qc-subject "${SUBJECT}"
+    fi
 }
 
 # SCRIPT STARTS HERE
 # ======================================================================================================================
 # Retrieve input params
-SUBJECT=$1
+SUBJECT=${1:0:-7}
+ses="ses-01"
 # get starting time:
 start=$(date +%s)
 
@@ -95,8 +96,13 @@ sct_check_dependencies -short
 # Go to folder where data will be copied and processed
 cd $PATH_DATA_PROCESSED
 # Copy source images
-rsync -avzh "$PATH_DATA"/"$SUBJECT" .
-
+rsync -avzh "$PATH_DATA"/"${SUBJECT}" .
+echo subjname
+echo ${SUBJECT}
+echo path of data processed
+echo $PATH_DATA_PROCESSED
+echo path of data??
+echo $PATH_DATA
 
 # T2w Segmentation and CSA
 # ======================================================================================================================
@@ -123,7 +129,7 @@ sct_process_segmentation -i "${file_t2_seg}.nii.gz" -vert 2:3 -vertfile label_T2
 
 # ======================================================================================================================
 #T2 Star (ME-GRE)
-file_t2s="${SUBJECT}_ses-01_run-1_T2starw"
+file_t2s="${SUBJECT}_${ses}_run-1_T2starw"
 
 #segment if does not exist
 segment_if_does_not_exist "${file_t2s}" "t2s"
@@ -136,63 +142,112 @@ sct_deepseg_gm -i "${file_t2s}".nii.gz -qc "$PATH_QC"
 # Register template->t2s (using warping field generated from template<->t2 registration)
 mkdir -p label_T2Star
 sct_register_multimodal -i "${SCT_DIR}"/data/PAM50/template/PAM50_t2s.nii.gz \
--iseg "${SCT_DIR}"/data/PAM50/template/PAM50_cord.nii.gz \
--d "${file_t2s}".nii.gz \
--dseg "${file_t2_seg}".nii.gz \
--param step=1,type=seg,algo=centermass:step=2,type=seg,algo=bsplinesyn,slicewise=1,iter=3:step=3,type=im,algo=syn,slicewise=1,iter=1,metric=CC \
--initwarp ./warp_template2anat.nii.gz \
--initwarpinv ./warp_anat2template.nii.gz \
--owarp ./warp_template2t2s.nii.gz \
--owarpinv ./warp_t2s2template.nii.gz
+			-iseg "${SCT_DIR}"/data/PAM50/template/PAM50_cord.nii.gz \
+			-d "${file_t2s}".nii.gz \
+			-dseg "${file_t2_seg}".nii.gz \
+			-param step=1,type=seg,algo=centermass:step=2,type=seg,algo=bsplinesyn,slicewise=1,iter=3:step=3,type=im,algo=syn,slicewise=1,iter=1,metric=CC \
+			-initwarp ./warp_template2anat.nii.gz \
+			-initwarpinv ./warp_anat2template.nii.gz \
+			-owarp ./warp_template2t2s.nii.gz \
+			-owarpinv ./warp_t2s2template.nii.gz
 
 # Warp template
 sct_warp_template -d "${file_t2s}".nii.gz \
--w ./warp_template2t2s.nii.gz
+		  -w ./warp_template2t2s.nii.gz
 
 # Subtract GM segmentation from cord segmentation to obtain WM segmentation
 sct_maths -i ./"${file_t2s}"_seg.nii.gz \
--sub ./"${file_t2s}"_gmseg.nii.gz \
--o ./"${file_t2s}"_wmseg.nii.gz
+	  -sub ./"${file_t2s}"_gmseg.nii.gz \
+	  -o ./"${file_t2s}"_wmseg.nii.gz
 # Compute cross-sectional area of the gray and white matter between C2 and C5
 sct_process_segmentation -i ./"${file_t2s}"_wmseg.nii.gz \
--vert 2:5 \
--perlevel 1 \
--o ${PATH_RESULTS}/t2s_csa_wm.csv -append 1
+			 -vert 2:5 \
+			 -perlevel 1 \
+			 -o ${PATH_RESULTS}/t2s_csa_wm.csv -append 1
 
 sct_process_segmentation -i ./"${file_t2s}"_gmseg.nii.gz \
--vert 2:5 \
--perlevel 1 \
--o ${PATH_RESULTS}/t2s_csa_gm.csv -append 1 
+			 -vert 2:5 \
+			 -perlevel 1 \
+			 -o ${PATH_RESULTS}/t2s_csa_gm.csv -append 1 
+
+#QSM_spinal_cord.
+# ===========================================================================================
+cd ../qsm/
+file_qsm_m="${SUBJECT}_${ses}_qsm_mag.nii.gz"
+file_qsm_p="${SUBJECT}_${ses}_qsm_final.nii.gz"
+
+#preprocessing of Mag image - use T1w as base. 
+segment_if_does_not_exist "${file_qsm_m}" "t1"
+file_qsm_seg="${FILESEG}"
+# Create labels in the cord at C2 and C5 mid-vertebral levels (only if it does not exist)
+label_if_does_not_exist "${file_qsm_m}" "${file_qsm_seg}"
+file_label="${FILELABEL}"
+#this might not work or be necessary but worth a shot. 
+
+
+#bring the Mag into the space of the template for downstream stats. Take the QSM output with it.
+#first crop the SC for ease of reg. ##TODO check if naming is correct for t1_seg
+sct_create_mask -i ${file_qsm_m} -p centerline,${file_qsm_seg} -size 35mm -f cylinder -o mask_qsm_m.nii.gz
+sct_create_mask -i ${file_qsm_p} -p centerline,${file_qsm_seg} -size 35mm -f cylinder -o mask_qsm_p.nii.gz
+#crop
+sct_crop_image -i ${file_qsm_m}.nii.gz -m mask_qsm_m.nii.gz
+sct_crop_image -i ${file_qsm_p}.nii.gz -m mask_qsm_p.nii.gz
+
+
+# Register template->qsm via t2s to account for GM segmentation
+#: the flag “-initwarpinv" provides a transformation qsm->template, in case you would like to bring all your qsm
+#       metrics in the PAM50 space (e.g. group averaging of maps)
+sct_register_multimodal -i "${SCT_DIR}/data/PAM50/template/PAM50_t1.nii.gz" \
+			-iseg "${SCT_DIR}/data/PAM50/template/PAM50_cord.nii.gz" \
+			-d crop_"${file_qsm_m}".nii.gz \
+			-dseg ${file_qsm_seg} \
+			-initwarp ../anat/warp_template2t2s.nii.gz \
+			-initwarpinv ../anat/warp_t2s2template.nii.gz \
+			-owarp warp_template2qsm.nii.gz \
+			-owarpinv warp_qsm2template.nii.gz \
+			-param step=1,type=seg,algo=centermass:step=2,type=seg,algo=bsplinesyn,slicewise=1,iter=3 \
+			-qc "$PATH_QC"
+
+
+# Warp template
+sct_warp_template -d crop_"${file_qsm_m}".nii.gz \
+		  -w ./warp_template2qsm_m.nii.gz
+sct_warp_template -d crop_"${file_qsm_q}".nii.gz \
+		  -w ./warp_template2qsm_p.nii.gz
+
+# compute qsm in dorsal columns between levels C2 and T1 (append across subjects)
+sct_extract_metric -i crop_${file_qsm_q}.nii.gz -f label_qsm_/atlas -vert 2:8 -vertfile label_qsm_mag/template/PAM50_levels.nii.gz \
+                   -perlevel 1 -method map -o "${PATH_RESULTS}/QSM_in_labels.csv" -append 1
 
 
 # dmri
 # ===========================================================================================
 
 cd ../dwi/
-file_dwi="${SUBJECT}_ses-01_run-1_dwi"
+file_dwi="${SUBJECT}_${ses}_run-1_dwi"
 
 # Preprocessing steps
 # Compute mean dMRI from dMRI data
 sct_dmri_separate_b0_and_dwi -i "${file_dwi}".nii.gz \
--bvec "${file_dwi}".bvec 
+			     -bvec "${file_dwi}".bvec 
 
 # Segment SC on mean dMRI data
 # Note: This segmentation does not need to be accurate-- it is only used to create a mask around the cord
 sct_deepseg_sc -i "${file_dwi}"_dwi_mean.nii.gz \
--c dwi \
--qc "${PATH_QC}"
+	       -c dwi \
+	       -qc "${PATH_QC}"
 
 # Create mask (for subsequent cropping)
 sct_create_mask -i "${file_dwi}"_dwi_mean.nii.gz \
--p centerline,"${file_dwi}_dwi_mean_seg.nii.gz" \
--f cylinder \
--size 35mm
+		-p centerline,"${file_dwi}_dwi_mean_seg.nii.gz" \
+		-f cylinder \
+		-size 35mm
 
 # Motion correction (moco)
 sct_dmri_moco -i "${file_dwi}".nii.gz \
--m mask_"${file_dwi}"_dwi_mean.nii.gz \
--bvec "${file_dwi}".bvec \
--qc "$PATH_QC" -qc-seg "${file_dwi}"_dwi_mean_seg.nii.gz
+	      -m mask_"${file_dwi}"_dwi_mean.nii.gz \
+	      -bvec "${file_dwi}".bvec \
+	      -qc "$PATH_QC" -qc-seg "${file_dwi}"_dwi_mean_seg.nii.gz
 
 # Check results in the QC report
 
@@ -205,62 +260,62 @@ sct_deepseg_sc -i "${file_dwi}"_moco_dwi_mean.nii.gz -c dwi -qc "$PATH_QC"
 # Note: the flag “-initwarpinv" provides a transformation dmri->template, in case you would like to bring all your DTI
 #       metrics in the PAM50 space (e.g. group averaging of FA maps)
 sct_register_multimodal -i "${SCT_DIR}/data/PAM50/template/PAM50_t1.nii.gz" \
--iseg "${SCT_DIR}/data/PAM50/template/PAM50_cord.nii.gz" \
--d "${file_dwi}"_moco_dwi_mean.nii.gz \
--dseg "${file_dwi}"_moco_dwi_mean_seg.nii.gz \
--initwarp ../anat/warp_template2t2s.nii.gz \
--initwarpinv ../anat/warp_t2s2template.nii.gz \
--owarp warp_template2dmri.nii.gz \
--owarpinv warp_dmri2template.nii.gz \
--param step=1,type=seg,algo=centermass:step=2,type=seg,algo=bsplinesyn,slicewise=1,iter=3 \
--qc "$PATH_QC"
+			-iseg "${SCT_DIR}/data/PAM50/template/PAM50_cord.nii.gz" \
+			-d "${file_dwi}"_moco_dwi_mean.nii.gz \
+			-dseg "${file_dwi}"_moco_dwi_mean_seg.nii.gz \
+			-initwarp ../anat/warp_template2t2s.nii.gz \
+			-initwarpinv ../anat/warp_t2s2template.nii.gz \
+			-owarp warp_template2dmri.nii.gz \
+			-owarpinv warp_dmri2template.nii.gz \
+			-param step=1,type=seg,algo=centermass:step=2,type=seg,algo=bsplinesyn,slicewise=1,iter=3 \
+			-qc "$PATH_QC"
 
 # Warp template (so 'label/atlas' can be used to extract metrics)
 sct_warp_template -d "${file_dwi}"_moco_dwi_mean.nii.gz \
--w warp_template2dmri.nii.gz \
--qc "$PATH_QC"
+		  -w warp_template2dmri.nii.gz \
+		  -qc "$PATH_QC"
 # Check results in the QC report
 
 # Compute DTI metrics using dipy [1]
 sct_dmri_compute_dti -i "${file_dwi}"_moco.nii.gz \
--bval "${file_dwi}".bval \
--bvec "${file_dwi}".bvec
+		     -bval "${file_dwi}".bval \
+		     -bvec "${file_dwi}".bvec
 # Tips: the flag "-method restore" estimates the tensor with robust fit (RESTORE method [2])
 
 # Compute FA within the white matter from individual level 2 to 5
 sct_extract_metric -i dti_FA.nii.gz \
--f label/atlas \
--l 51 -method map \
--vert 2:5 \
--vertfile label/template/PAM50_levels.nii.gz \
--perlevel 1 \
--o fa_in_wm.csv
+		   -f label/atlas \
+		   -l 51 -method map \
+		   -vert 2:5 \
+		   -vertfile label/template/PAM50_levels.nii.gz \
+		   -perlevel 1 \
+		   -o fa_in_wm.csv
 
 # Compute DTI metrics
 # Tips: The flag -method "restore" allows you to estimate the tensor with robust fit (see: sct_dmri_compute_dti -h)
 sct_dmri_compute_dti -i "${file_dwi}"_moco.nii.gz \
--bval "${file_dwi}".bval \
--bvec "${file_dwi}".bvec
+		     -bval "${file_dwi}".bval \
+		     -bvec "${file_dwi}".bvec
 
 # Compute FA within right and left lateral corticospinal tracts from slices 2 to 14 using weighted average method
 sct_extract_metric -i dti_FA.nii.gz \
--z 2:14 \
--method wa \
--l 4,5 \
--o fa_in_cst.csv
+		   -z 2:14 \
+		   -method wa \
+		   -l 4,5 \
+		   -o fa_in_cst.csv
 
 # Bring metric to template space (e.g. for group mapping)
 sct_apply_transfo -i dti_FA.nii.gz \
--d "$SCT_DIR"/data/PAM50/template/PAM50_t2.nii.gz \
--w warp_dmri2template.nii.gz
+		  -d "$SCT_DIR"/data/PAM50/template/PAM50_t2.nii.gz \
+		  -w warp_dmri2template.nii.gz
 # Go back to root folder
 cd ../anat
 
 # MT
 # ======================================================================================================================
 
-file_mt1="${SUBJECT}_ses-01_acq-MTon_run-1_MTS"
-file_mt0="${SUBJECT}_ses-01_acq-MToff_run-1_MTS"
+file_mt1="${SUBJECT}_${ses}_acq-MTon_run-1_MTS"
+file_mt0="${SUBJECT}_${ses}_acq-MToff_run-1_MTS"
 # Segment spinal cord
 segment_if_does_not_exist "${file_mt1}" "t2s"
 file_mt1_seg=$FILESEG
@@ -304,13 +359,13 @@ sct_extract_metric -i mtr.nii.gz -f label_MT/atlas -l 53 -vert 2:5 -vertfile lab
 # Verify presence of output files and write log file if error
 # ======================================================================================================================
 FILES_TO_CHECK=(
-  "$file_t2_seg.nii.gz"
-  "mtr.nii.gz"
+    "$file_t2_seg.nii.gz"
+    "mtr.nii.gz"
 )
 for file in "${FILES_TO_CHECK[@]}"; do
-  if [ ! -e "${file}" ]; then
-    echo "${SUBJECT}/${file} does not exist" >> "${PATH_LOG}/error.log"
-  fi
+    if [ ! -e "${file}" ]; then
+	echo "${SUBJECT}/${file} does not exist" >> "${PATH_LOG}/error.log"
+    fi
 done
 
 # Display useful info for the log
--- run_sct_batch_script.sh
+++ run_sct_batch_script.sh
@@ -1,1 +1,2 @@
-sct_run_batch -script process_data.sh -path-data /90days/uqtshaw/BeLong/BeLong_SCT_BIDS/ -path-output /90days/uqtshaw//BeLong/BeLong_SCT_BIDS/derivatives/sct_output/ 
+sct_run_batch -script ./process_data.sh -path-data /90days/uqtshaw/BeLong/BeLong_SCT_BIDS/
+#-path-output /90days/uqtshaw/BeLong/BeLong_SCT_BIDS/data_processed/ 
