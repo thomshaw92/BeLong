4bda5a78f09421e86bdb247de1599fe9bb8ad9a7
thomshaw92 <thomasbasilshaw@gmail.com>
2022-09-27 13:42:50 +1000

    small fixes to file paths

--- move_qsm_processing_to_new_folders.sh
+++ move_qsm_processing_to_new_folders.sh
@@ -57,11 +57,11 @@ cp sub-021_ses-01_run-01_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average
 cp sub-021_ses-01_run-06_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-021/ses-01/qsm/qsmH.nii
 #all the same
 for x in sub-020 sub-022 sub-023 sub-024 sub-025 sub-026 sub-027 sub-028 sub-029 ; do 
-    cp ${x}_ses*_run-01_part_phase* ${SCT_dir}/${x}/ses-01/qsm/qsmH.nii
-    cp ${x}_ses*_run-04_part_phase* ${SCT_dir}/${x}/ses-01/qsm/qsmN.nii
+    cp ${x}_ses-01_run-01_echo-01_part-phase_MEGRE*.nii ${SCT_dir}/${x}/ses-01/qsm/qsmH.nii
+    cp ${x}_ses-01_run-04_echo-01_part-phase_MEGRE*.nii ${SCT_dir}/${x}/ses-01/qsm/qsmN.nii
 done
 
-
+#sub-024_ses-01_run-01_echo-13_part-phase_MEGRE.nii sub-024_ses-01_run-01_echo-01_part-phase_MEGRE.nii
 ###################
 
 #magnitude images
@@ -70,55 +70,55 @@ done
 
 
 cd /90days/uqtshaw/BeLong/BeLong_QSMxT_BIDS/01_qsmxt_niftis/
-#sub-005/ses-2/anat/sub-005_ses-2_acq-qsmE01_run-1_magnitude.nii.gz
+#/sub-005/ses-3/anat/sub-005_ses-3_acq-qsmE01_run-1_magnitude.nii.gz
 #head		
-cp sub-005/ses-3/anat/sub-005_ses-2_acq-qsmE01_run-1_magnitude.nii.gz ${SCT_dir}/sub-005/ses-01/qsm/qsmHM.nii.gz
+cp sub-005/ses-3/anat/sub-005_ses-3_acq-qsmE01_run-1_magnitude.nii.gz ${SCT_dir}/sub-005/ses-01/qsm/qsmHM.nii.gz
 #Neck
-cp sub-005/ses-5/anat/sub-005_ses-5_acq-qsmE01_run-1_magnitude.nii.gz ${SCT_dir}/sub-005/ses-01/qsm/qsmNM.nii.gz
+cp sub-005/ses-5/anat/sub-005_ses-5_acq-qsm_run-1_magnitude.nii.gz ${SCT_dir}/sub-005/ses-01/qsm/qsmNM.nii.gz
 #H&N
-cp sub-006/ses-2/anat/_acq-qsmE01_run-1_phase_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-006/ses-01/qsm/qsmHM.nii.gz
+cp sub-006/ses-2/anat/*E01_run-1_magnitude.nii.gz ${SCT_dir}/sub-006/ses-01/qsm/qsmHM.nii.gz
 #no neck for 6
 #head
-cp sub-007/ses-1/anat/_acq-qsmE01_run-1_phase_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-006/ses-01/qsm/qsmHM.nii.gz
+cp sub-007/ses-1/anat/*E01_run-1_magnitude.nii.gz ${SCT_dir}/sub-006/ses-01/qsm/qsmHM.nii.gz
 #neck
-cp sub-007/ses-10/anat/_acq-qsmE01_run-1_phase_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-007/ses-01/qsm/qsmNM.nii.gz
-#h&n
-cp sub-011/ses-01/anat/_run-01_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-011/ses-01/qsm/qsmHM.nii.gz
+cp sub-007/ses-10/anat/*E01_run-1_magnitude.nii.gz ${SCT_dir}/sub-007/ses-01/qsm/qsmNM.nii.gz
+#h&n   sub-015_ses-01_run-02_echo-01_part-mag_MEGRE.nii
+cp sub-011/ses-01/anat/sub-011_ses-01_run-01_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-011/ses-01/qsm/qsmHM.nii
 #no neck for 11
 #H&N
-cp sub-013/ses-01/anat/_run-01_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-013/ses-01/qsm/qsmHM.nii.gz
+cp sub-013/ses-01/anat/sub-013_ses-01_run-01_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-013/ses-01/qsm/qsmHM.nii
 #N
-cp sub-013/ses-01/anat/_run-03_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-013/ses-01/qsm/qsmNM.nii.gz
+cp sub-013/ses-01/anat/sub-013_ses-01_run-03_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-013/ses-01/qsm/qsmNM.nii
 #H&N
-cp sub-014/ses-01/anat/_run-01_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-014/ses-01/qsm/qsmHM.nii.gz
+cp sub-014/ses-01/anat/sub-014_ses-01_run-01_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-014/ses-01/qsm/qsmHM.nii
 #N
-cp sub-014/ses-01/anat/_run-03_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-014/ses-01/qsm/qsmNM.nii.gz
+cp sub-014/ses-01/anat/*_run-03_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-014/ses-01/qsm/qsmNM.nii
 #H&N
-cp sub-015/ses-01/anat/_run-01_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-015/ses-01/qsm/qsmHM.nii.gz
+cp sub-015/ses-01/anat/*_run-01_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-015/ses-01/qsm/qsmHM.nii
 #N 
-cp sub-015/ses-01/anat/_run-03_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-015/ses-01/qsm/qsmNM.nii.gz
+cp sub-015/ses-01/anat/*_run-03_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-015/ses-01/qsm/qsmNM.nii
 #H&N
-cp sub-016/ses-01/anat/_run-03_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-016/ses-01/qsm/qsmHM.nii.gz
+cp sub-016/ses-01/anat/*_run-03_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-016/ses-01/qsm/qsmHM.nii
 #n
-cp sub-016/ses-01/anat/_run-05_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-016/ses-01/qsm/qsmNM.nii.gz
+cp sub-016/ses-01/anat/*_run-05_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-016/ses-01/qsm/qsmNM.nii
 #HN/anat/
-cp sub-017/ses-01/anat/_run-01_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-017/ses-01/qsm/qsmHM.nii.gz
+cp sub-017/ses-01/anat/*_run-01_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-017/ses-01/qsm/qsmHM.nii
 #N
-cp sub-017/ses-01/anat/_run-07_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-017/ses-01/qsm/qsmNM.nii.gz
+cp sub-017/ses-01/anat/*_run-07_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-017/ses-01/qsm/qsmNM.nii
 #H&N
-cp sub-018/ses-01/anat/_run-01_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-018/ses-01/qsm/qsmHM.nii.gz
+cp sub-018/ses-01/anat/*_run-01_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-018/ses-01/qsm/qsmHM.nii
 #N
-cp sub-018/ses-01/anat/_run-08_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-018/ses-01/qsm/qsmNM.nii.gz
+cp sub-018/ses-01/anat/*_run-08_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-018/ses-01/qsm/qsmNM.nii
 #HN
-cp sub-019/ses-01/anat/_run-01_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-019/ses-01/qsm/qsmHM.nii.gz
+cp sub-019/ses-01/anat/*_run-01_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-019/ses-01/qsm/qsmHM.nii
 #N
-cp sub-019/ses-01/anat/_run-04_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-019/ses-01/qsm/qsmNM.nii.gz
+cp sub-019/ses-01/anat/*_run-04_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-019/ses-01/qsm/qsmNM.nii
 #HN
-cp sub-021/ses-01/anat/_run-01_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-021/ses-01/qsm/qsmHM.nii.gz
+cp sub-021/ses-01/anat/*_run-01_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-021/ses-01/qsm/qsmHM.nii
 #N
-cp sub-021ses-01/anat/_run-06_echo-01_part-phase_MEGRE_scaled_qsm_000_twopass_average.nii ${SCT_dir}/sub-021/ses-01/qsm/qsmHM.nii.gz
+cp sub-021/ses-01/anat/*_run-06_echo-01_part-mag_MEGRE.nii ${SCT_dir}/sub-021/ses-01/qsm/qsmHM.nii
 #all the same
 for x in sub-020 sub-022 sub-023 sub-024 sub-025 sub-026 sub-027 sub-028 sub-029 ; do 
-    cp ${x}_ses*_run-01_part_phase* ${SCT_dir}/${x}/ses-01/qsm/qsmHM.nii.gz
-    cp ${x}_ses*_run-04_part_phase* ${SCT_dir}/${x}/ses-01/qsm/qsmNM.nii.gz
+    cp ${x}/ses-01/anat/${x}_ses*_run-01_echo-01_part-mag_MEGRE.nii ${SCT_dir}/${x}/ses-01/qsm/qsmHM.nii
+    cp ${x}/ses-01/anat/${x}_ses*_run-04_echo-01_part-mag_MEGRE.nii ${SCT_dir}/${x}/ses-01/qsm/qsmNM.nii
 done
